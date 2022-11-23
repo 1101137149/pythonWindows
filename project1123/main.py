9797bd819220796bb222e74d1e799d3ac511a8d0
Your Name1101137149 <fgd8389@gmail.com>
2022-11-23 14:09:48 +0800

    api

--- project1123/main.py
+++ project1123/main.py
@@ -1,6 +1,11 @@
+import datasource as ds
+
 
 def main():
     print("這裡是main function的執行點")
+    all_data=ds.get_forcast_data(ds.tw_county_names["基隆"])
+    for item in all_data:
+        print(item['dt_txt'])
 
 if __name__=="__main__":
     print("這裡是程式的執行點")
