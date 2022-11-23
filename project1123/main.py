9662f8672766419cd3394db1c2ab3fbd5bd0a8ea
Your Name1101137149 <fgd8389@gmail.com>
2022-11-23 15:32:53 +0800

    tkinter建立視窗

--- project1123/main.py
+++ project1123/main.py
@@ -1,8 +1,20 @@
 import datasource as ds
 from secrets import api_key
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
 
 def main():
+    window=Window()
+    window.title("各縣市4天天氣預測")
+    window.mainloop()
+
+
+
+'''
     print("這裡是main function的執行點")
     try:
         list_data=ds.get_forcast_data(ds.tw_county_names["金門"],api_key)
@@ -12,7 +24,7 @@ def main():
     
     for item in list_data:
         print(item['dt_txt'])
-
+'''
 if __name__=="__main__":
     print("這裡是程式的執行點")
     main()
